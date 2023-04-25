fb16f71d8093ab6371645500207bb5f8647283de
Kobra93 <nive54@gmail.com>
2023-04-26 01:38:26 +0200

    Dodanie metody recordResult dla klasy RankingTable

--- task3.php
+++ task3.php
@@ -8,3 +8,10 @@ class RankingTable {
             $this->scores[$player] = array('score' => 0, 'games' => 0);
         }
     }
+    public function recordResult($player, $score) {
+        if (!array_key_exists($player, $this->scores)) {
+            throw new Exception('Unknown player');
+        }
+        $this->scores[$player]['score'] += $score;
+        $this->scores[$player]['games']++;
+    }
