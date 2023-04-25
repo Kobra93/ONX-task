0e70fcd66b9c431ef49de5a3ba04a02c97ff5d68
Kobra93 <nive54@gmail.com>
2023-04-25 22:56:15 +0200

    Dodanie kodu sprawdzającego funkcję

--- task1.php
+++ task1.php
@@ -9,3 +9,10 @@ class Pipeline {
     };
   }
 }
+$pipe = Pipeline::make(
+  function($var) { return $var * 3; },
+  function($var) { return $var + 1; },
+  function($var) { return $var / 2; }
+);
+
+echo $pipe(3); // 5
