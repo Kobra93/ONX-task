90a69216191913d47aa3898f90a78eddf977569b
Kobra93 <nive54@gmail.com>
2023-04-25 22:26:02 +0200

    Dodanie klasy Pipeline

---
+++ task.php
@@ -0,0 +1,19 @@
+class Pipeline {
+  public static function make(...$functions) {
+    return function($arg) use ($functions) {
+      $result = $arg;
+      foreach ($functions as $function) {
+        $result = $function($result);
+      }
+      return $result;
+    };
+  }
+}
+
+$pipe = Pipeline::make(
+  function($var) { return $var * 3; },
+  function($var) { return $var + 1; },
+  function($var) { return $var / 2; }
+);
+
+echo $pipe(3); // 5
