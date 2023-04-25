5bdd371c82e2e6b088ae0888033fcf1ecbcd7cb3
Kobra93 <nive54@gmail.com>
2023-04-25 22:39:15 +0200

    Dodanie metody make

--- task1.php
+++ task1.php
@@ -1,3 +1,11 @@
 class Pipeline {
   public static function make(...$functions) {
-    
+    return function($arg) use ($functions) {
+      $result = $arg;
+      foreach ($functions as $function) {
+        $result = $function($result);
+      }
+      return $result;
+    };
+  }
+}
