6268b2a48a4e8c54a7bebfced7ed11ec239f77c2
Kobra93 <nive54@gmail.com>
2023-04-27 23:48:29 +0200

    Uzupełnienie modeli o relacje

--- laravel1.php
+++ laravel1.php
@@ -3,15 +3,34 @@ Schema::create('clients', function (Blueprint $table) {
     $table->string('name');
     $table->timestamps();
 });
+public function employees()
+{
+    return $this->hasMany(Employee::class);
+}
+
 Schema::create('employees', function (Blueprint $table) {
     $table->id();
     $table->string('name');
     $table->foreignId('client_id')->constrained();
     $table->timestamps();
 });
+public function client()
+{
+    return $this->belongsTo(Client::class);
+}
+
+public function orders()
+{
+    return $this->hasMany(Order::class);
+}
+
 Schema::create('orders', function (Blueprint $table) {
     $table->id();
     $table->string('product_name');
     $table->foreignId('employee_id')->constrained();
     $table->timestamps();
 });
+public function employee()
+{
+    return $this->belongsTo(Employee::class);
+}
