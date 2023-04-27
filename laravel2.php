f5b347ff1a5d3d78f35a80ac12a26236b35381ee
Kobra93 <nive54@gmail.com>
2023-04-28 01:57:09 +0200

    Dodanie modeli

--- laravel2.php
+++ laravel2.php
@@ -5,3 +5,22 @@ Schema::create('user_cars', function (Blueprint $table) {
     $table->boolean('is_in_use')->default(false);
     $table->timestamps();
 });
+class User extends Authenticatable
+{
+    use HasFactory, Notifiable;
+
+    public function cars()
+    {
+        return $this->belongsToMany(Car::class)->withPivot('is_in_use');
+    }
+}
+
+class Car extends Model
+{
+    use HasFactory;
+
+    public function users()
+    {
+        return $this->belongsToMany(User::class)->withPivot('is_in_use');
+    }
+}
