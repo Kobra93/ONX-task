3473737e68a039803bc44434dcafd144eb1a3bff
Kobra93 <nive54@gmail.com>
2023-04-28 02:01:53 +0200

    Dodanie endpointów do kontrolera

--- laravel2.php
+++ laravel2.php
@@ -24,3 +24,35 @@ class Car extends Model
         return $this->belongsToMany(User::class)->withPivot('is_in_use');
     }
 }
+class UserCarController extends Controller
+{
+    public function assignCar(Request $request)
+    {
+        $user = User::findOrFail($request->user_id);
+        $car = Car::findOrFail($request->car_id);
+
+        $user->cars()->attach($car, ['is_in_use' => true]);
+
+        return response()->json(['message' => 'Car assigned successfully']);
+    }
+
+    public function unassignCar(Request $request)
+    {
+        $user = User::findOrFail($request->user_id);
+        $car = Car::findOrFail($request->car_id);
+
+        $user->cars()->updateExistingPivot($car->id, ['is_in_use' => false]);
+
+        return response()->json(['message' => 'Car unassigned successfully']);
+    }
+
+    public function isCarInUse(Request $request)
+    {
+        $user = User::findOrFail($request->user_id);
+        $car = Car::findOrFail($request->car_id);
+
+        $isInUse = $user->cars()->where('car_id', $car->id)->first()->pivot->is_in_use;
+
+        return response()->json(['is_in_use' => $isInUse]);
+    }
+}
